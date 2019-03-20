f6683751aabc327ce9f4bbe4a41c9b6a2f706c93
Andrew <andrew.batzel@gmail.com>
2019-03-19 23:39:17 -0600

    Added better satellite propagation logic

--- app.py
+++ app.py
@@ -1,76 +1,47 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 from flask_cors import CORS
-from flask_socketio import SocketIO
-import time
-import random
 import logging
 import math
 
 from satellite import Satellite
 
 app = Flask(__name__)
-CORS(app)
-params = {"ping_timeout": 60 * 60}
-socketio = SocketIO(app, **params)
-logging.basicConfig(filename='B:\\UCCS\\Web\\cs5260\\server\\satellite.log', filemode='w', level=logging.DEBUG, format='%(name)s - %(levelname)s - %(message)s')
+CORS(app, resources={r"/api/*": {"origins": "*"}})
+# logging.basicConfig(filename='B:\\UCCS\\Web\\cs5260\\server\\satellite.log', filemode='w', level=logging.DEBUG, format='%(name)s - %(levelname)s - %(message)s')
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
 
-@socketio.on('points')
-def points():
-    lat = 0
-    long = 0
-    while True:
-        point = {'name': 'test1', 'longitude': lat, 'latitude': long, 'height': 1000000, 'size': 10}
-        socketio.emit('points', point)
-        print('sent point')
-        lat = lat + 1
-        long = long + 1
-        time.sleep(10)
-
-def rand_lat():
-    return random.randint(0, 90)
-
-def rand_long():
-    return random.randint(0, 180)
-
-def rand_height():
-    return random.randint(3000000, 5000000)
-
 sats = []
-line1 = '1 00005U 58002B   00179.78495062  .00000023  00000-0  28098-4 0  4753'
-line2 = '2 00005  34.2682 348.7242 1859667 331.7664  19.3264 10.82419157413667'
+line1 = '1 25590U 98076A   19064.91273959  .00000018  00000-0  36015-5 0  9995'
+line2 = '2 25590  82.9374  12.7710 0030482 304.3953 113.2645 13.72960770 11936'
 sat1 = Satellite(id='sat1', size=10, line1=line1, line2=line2)
 sats.append(sat1)
-tleLine1 = '1 25544U 98067A   13149.87225694  .00009369  00000-0  16828-3 0  9031'
-tleLine2 = '2 25544 051.6485 199.1576 0010128 012.7275 352.5669 15.50581403831869'
+tleLine1 = '1 26818U 01023A   19064.84364637  .00000050  00000-0  37736-4 0  9997'
+tleLine2 = '2 26818  82.9288  33.0547 0031430 254.5964 105.1727 13.73931458889587'
 sat2 = Satellite(id='sat2', size=10, line1=tleLine1, line2=tleLine2)
 sats.append(sat2)
-tl1 = '1 17181U 86096A   19061.54477344 -.00000327  00000-0  00000+0 0  9994'
-tl2 = '2 17181  14.6347   0.1098 0002456 287.7661 168.6772  1.00266807125563'
-sat3 = Satellite(id='sat3', size=10, line1=tl1, line2=tl2)
-sats.append(sat3)
 
 def line(satells, time):
     dist = distance(satells[0].get_propagation(time)[0], satells[1].get_propagation(time)[0])
-    logging.debug('Distance [{}]'.format(dist))
-    if dist <= 10000000:
-        return True
-    return False
+    if dist <= 3000000:
+        return [{'id': '{}-{}'.format(satells[0].id, satells[1].id)}]
+    return []
 
 def distance(p_1, p_2):
-    return math.sqrt(math.pow(p_1[0] - p_2[0], 2) + math.pow(p_1[1] - p_2[1], 2) + math.pow(p_1[2] - p_2[2], 2)) * 1000
+    return math.sqrt(math.pow(p_1[0] * 1000 - p_2[0] * 1000, 2) + math.pow(p_1[1] * 1000 - p_2[1] * 1000, 2) + math.pow(p_1[2] * 1000 - p_2[2] * 1000, 2))
 
-@socketio.on('satellite')
-def satellite(clock_time):
+@app.route('/api/satellite',methods=['POST'])
+def satellite():
+    clock_time = request.get_json()['time']
     strptime = datetime.strptime(clock_time, '%Y-%m-%dT%H:%M:%S.%f%z')
     sats_ = [x.get_position(strptime) for x in sats]
-    socketio.emit('satellite', {'satellites': sats_, 'lines': False})
-    time.sleep(1/100)
+    l = line(sats, strptime)
+    return jsonify({'satellites': sats_, 'lines': l})
+
 
 if __name__ == '__main__':
-    socketio.run(app)
+    app.run()
--- satellite.py
+++ satellite.py
@@ -1,14 +1,17 @@
-from sgp4.earth_gravity import wgs72
+from sgp4.earth_gravity import wgs84
 from sgp4.io import twoline2rv
 import math
+import pyproj
 import logging
 
 class Satellite:
     def __init__(self, id, size, line1, line2):
-        self.satellite = twoline2rv(line1, line2, wgs72)
+        self.satellite = twoline2rv(line1, line2, wgs84)
         self.id = id
         self.size = size
         self.scale = 10
+        self.ecef = pyproj.Proj(proj='geocent', ellps='WGS84', datum='WGS84')
+        self.lla = pyproj.Proj(proj='latlong', ellps='WGS84', datum='WGS84')
 
     def get_propagation(self, time):
         position, velocity = self.satellite.propagate(time.year, time.month, time.day, time.hour, time.minute,
@@ -16,17 +19,6 @@ class Satellite:
         return position, velocity
 
     def get_position(self, time):
-        # position, velocity = self.satellite.propagate(time['year'], time['month'], time['day'], time['hour'], time['minute'], time['second'])
         position, velocity = self.get_propagation(time)
-        radius = math.sqrt(math.pow(position[0], 2) + math.pow(position[1], 2) + math.pow(position[2], 2)) * 1000
-        polar = math.acos(position[2] / radius)
-        azimuth = math.atan2(position[1], position[0])
-
-        latitude = math.degrees(polar - math.radians(90))
-        longitude = math.degrees(azimuth)
-
-        # logging.debug('time [{}, {}, {}, {}, {}, {}'.format(time.year, time.month, time.day, time.hour, time.minute, time.second))
-        # logging.debug('position [{},{},{}]'.format(position[0], position[1], position[2]))
-
+        longitude, latitude, radius = pyproj.transform(self.ecef, self.lla, position[0] * 1000, position[1] * 1000, position[2] * 1000, radians=False)
         return {'latitude': latitude, 'longitude': longitude, 'height': radius, 'size': self.size, 'id': self.id}
-
